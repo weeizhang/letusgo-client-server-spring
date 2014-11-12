809b1bbf863394b89c0cd9b7b7271dd1b5999407
lzhangwei <zhangwei_999@foxmail.com>
2014-11-12 16:00:15 +0800

    add fields

--- src/main/java/com/thoughtworks/letusgo/entity/Category.java
+++ src/main/java/com/thoughtworks/letusgo/entity/Category.java
@@ -1,4 +1,11 @@
 package com.thoughtworks.letusgo.entity;
 
 public class Category {
+    private int id;
+    private String name;
+
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
--- src/main/java/com/thoughtworks/letusgo/entity/Item.java
+++ src/main/java/com/thoughtworks/letusgo/entity/Item.java
@@ -11,4 +11,32 @@ public class Item{
     private Category category;
     private int discount;
     private List<Promotion> promotionList;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
 }
