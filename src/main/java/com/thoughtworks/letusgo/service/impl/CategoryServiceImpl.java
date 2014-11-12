e535d40ccb99db90a4b34ad9542544ad4ca464b3
lzhangwei <zhangwei_999@foxmail.com>
2014-11-13 00:19:48 +0800

    add constructor

--- src/main/java/com/thoughtworks/letusgo/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/thoughtworks/letusgo/service/impl/CategoryServiceImpl.java
@@ -1,6 +1,20 @@
 package com.thoughtworks.letusgo.service.impl;
 
+import com.thoughtworks.letusgo.dao.CategoryDao;
 import com.thoughtworks.letusgo.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CategoryServiceImpl implements CategoryService {
+
+    @Autowired
+    private CategoryDao categoryDao;
+
+    public CategoryServiceImpl() {
+    }
+
+    public CategoryServiceImpl(CategoryDao categoryDao) {
+        this.categoryDao = categoryDao;
+    }
 }
