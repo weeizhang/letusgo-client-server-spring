9f65b1ff70fdcb0f3997679bdd60226545a4d6bb
lzhangwei <zhangwei_999@foxmail.com>
2014-11-13 00:00:51 +0800

    add constructor

--- src/main/java/com/thoughtworks/letusgo/entity/Category.java
+++ src/main/java/com/thoughtworks/letusgo/entity/Category.java
@@ -4,6 +4,9 @@ public class Category{
     private int id;
     private String name;
 
+    public Category() {
+    }
+
     public Category(int id, String name) {
         this.id = id;
         this.name = name;
