aa219071fe38b978382cca81770ba3197564bb0b
lzhangwei <zhangwei_999@foxmail.com>
2014-11-12 22:22:44 +0800

    add items/barcode

--- src/main/java/com/thoughtworks/letusgo/controller/ItemController.java
+++ src/main/java/com/thoughtworks/letusgo/controller/ItemController.java
@@ -1,9 +1,28 @@
 package com.thoughtworks.letusgo.controller;
 
-import org.springframework.stereotype.Controller;
+import com.thoughtworks.letusgo.entity.Item;
+import com.thoughtworks.letusgo.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/items")
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/items")
 public class ItemController {
+    @Autowired
+    private ItemService itemService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Item> getItems() {
+        return itemService.getItems();
+    }
+
+    @RequestMapping(value="{barcode}", method = RequestMethod.GET)
+    public Item getItemByBarcode(@PathVariable String barcode) {
+        return itemService.getItemByBarcode(barcode);
+    }
 }
