480998f2513728e978c760ecb832609a18bfb137
lzhangwei <zhangwei_999@foxmail.com>
2014-11-12 16:25:14 +0800

    add ItemService

---
+++ src/main/java/com/thoughtworks/letusgo/service/ItemService.java
@@ -0,0 +1,9 @@
+package com.thoughtworks.letusgo.service;
+
+import com.thoughtworks.letusgo.entity.Item;
+
+import java.util.List;
+
+public interface ItemService {
+    List<Item> getItems();
+}
---
+++ src/main/java/com/thoughtworks/letusgo/service/impl/ItemServiceImpl.java
@@ -0,0 +1,15 @@
+package com.thoughtworks.letusgo.service.impl;
+
+import com.thoughtworks.letusgo.entity.Item;
+import com.thoughtworks.letusgo.service.ItemService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ItemServiceImpl implements ItemService{
+    @Override
+    public List<Item> getItems() {
+        return null;
+    }
+}
