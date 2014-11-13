1cccc52456af16b758bdb9794ce05a920d48febb
lzhangwei <zhangwei_999@foxmail.com>
2014-11-13 10:15:27 +0800

    update to restController

--- src/main/java/com/thoughtworks/letusgo/controller/CategoryController.java
+++ src/main/java/com/thoughtworks/letusgo/controller/CategoryController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping(value = "/categories")
 public class CategoryController {
     @Autowired
