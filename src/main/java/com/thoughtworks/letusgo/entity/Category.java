574ce5ace3a23d535e4da8fc04b6cf305473d164
lzhangwei <zhangwei_999@foxmail.com>
2014-11-14 20:43:15 +0800

    add setter

--- src/main/java/com/thoughtworks/letusgo/entity/Category.java
+++ src/main/java/com/thoughtworks/letusgo/entity/Category.java
@@ -19,4 +19,12 @@ public class Category{
     public String getName() {
         return name;
     }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
