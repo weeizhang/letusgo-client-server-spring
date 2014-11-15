2a49cbab98f17c24bb4ab531281ea51b3a426400
lzhangwei <zhangwei_999@foxmail.com>
2014-11-15 22:23:24 +0800

    add item controller test

--- src/test/java/com/thoughtworks/letusgo/controller/ItemControllerTests.java
+++ src/test/java/com/thoughtworks/letusgo/controller/ItemControllerTests.java
@@ -1,39 +1,69 @@
 package com.thoughtworks.letusgo.controller;
 
+import com.thoughtworks.letusgo.entity.Category;
+import com.thoughtworks.letusgo.entity.Item;
+import com.thoughtworks.letusgo.service.ItemService;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
+import org.springframework.test.util.ReflectionTestUtils;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+import java.util.ArrayList;
+import java.util.List;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@WebAppConfiguration
-@ContextConfiguration("file:src/main/webapp/WEB-INF/mvc-dispatcher-servlet.xml")
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 public class ItemControllerTests {
-    private MockMvc mockMvc;
+    private ItemController itemController;
 
-    @SuppressWarnings("SpringJavaAutowiringInspection")
-    @Autowired
-    protected WebApplicationContext wac;
+    private ItemService itemService;
+
+    private final List<Item> items = new ArrayList<Item>();
 
     @Before
-    public void setup() {
-        this.mockMvc = webAppContextSetup(this.wac).build();
+    public void before() {
+        itemService = mock(ItemService.class);
+        Item item1 = new Item(1, "Item000001", "雪碧", "瓶", 3.0, new Category(1, "饮料"));
+        Item item2 = new Item(2, "Item000002", "可乐", "瓶", 3.0, new Category(1, "饮料"));
+        items.add(item1);
+        items.add(item2);
+        when(itemService.getItems()).thenReturn(items);
+
+        when(itemService.getItemById(1)).thenReturn(item1);
+        itemController = new ItemController();
+
+        ReflectionTestUtils.setField(itemController, "itemService", itemService);
     }
 
-//    @Test
-//    public void get_items_test() throws Exception {
-//        mockMvc.perform(get("/items"))
-//                .andExpect(status().isOk())
-//                .andExpect(view().name("items"));
-//    }
+    @Test
+    public void should_return_all_items() {
+        assertThat(itemController.getItems().get(0).getName()).isEqualTo("雪碧");
+    }
+
+    @Test
+    public void should_return_item_by_id() {
+        assertThat(itemController.getItemById(1).getName()).isEqualTo("雪碧");
+    }
+
+    @Test
+    public void should_delete_item_by_id() {
+        itemController.deleteItemById(1);
+        verify(itemService).deleteItemById(1);
+    }
+
+    @Test
+    public void should_add_item() {
+        Item item = new Item(3, "Item000003", "果粒橙", "瓶", 3.0, new Category(1, "饮料"));
+        itemController.addItem(item);
+        verify(itemService).addItem(item);
+    }
+
+    @Test
+    public void should_update_item() {
+        Item item = new Item(2, "Item000002", "果粒橙", "瓶", 3.0, new Category(1, "饮料"));
+        itemController.updateItem(2, item);
+        verify(itemService).updateItem(2, item);
+    }
 }
