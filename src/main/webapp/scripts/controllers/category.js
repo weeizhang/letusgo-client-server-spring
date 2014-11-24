c7b6ace23cae1d20cc4bf741563287621e8bbc9e
lzhangwei <zhangwei_999@foxmail.com>
2014-11-24 22:47:38 +0800

    update authority manage

--- src/main/webapp/scripts/controllers/category.js
+++ src/main/webapp/scripts/controllers/category.js
@@ -2,80 +2,80 @@
 
 angular.module('letusgoApp')
     .controller('CategoryCtrl', function ($scope, $location, CategoryService) {
-        if($scope.user.rank === 1) {
+        if ($scope.user.rank === 1) {
             $location.path('/main');
             alert('您无权限进入管理模块！')
-        }
+        } else {
+            $scope.$emit('to-parent-manage');
+            $scope.categorys = [];
 
-        $scope.$emit('to-parent-manage');
-        $scope.categorys = [];
-
-        CategoryService.getAllCategoryInfo(function (data) {
-            $scope.categorys = data;
-        });
-        $scope.tip = '';
-
-        $scope.itemsPerPage = 5;
-        $scope.currentPage = 1;
-
-        $scope.pageCount = function () {
-            var maxPage = Math.ceil($scope.categorys.length / $scope.itemsPerPage);
-            return maxPage;
-        };
-
-        $scope.range = function () {
-            var pages = [];
-            var maxPage = $scope.pageCount();
-            for (var i = 1; i <= maxPage; i++) {
-                pages.push(i);
-            }
-            return pages;
-        };
-
-        $scope.prevPage = function () {
-            if ($scope.currentPage > 1) {
-                $scope.currentPage--;
-            }
-        };
-
-        $scope.prevPageDisabled = function () {
-            return $scope.currentPage === 1 ? true : false;
-        };
-
-        $scope.nextPage = function () {
-            if ($scope.currentPage < $scope.pageCount()) {
-                $scope.currentPage++;
-            }
-        };
-
-        $scope.nextPageDisabled = function () {
-            return $scope.currentPage === $scope.pageCount() ? true : false;
-        };
-
-        $scope.setPage = function (n) {
-            $scope.currentPage = n;
-        };
-
-
-        $scope.removeCategoryInfo = function (categoryInfo) {
-            CategoryService.removeCategoryInfo(categoryInfo, function (data) {
-                if (data === 'success') {
-                    $scope.tip = '';
-                    CategoryService.getAllCategoryInfo(function (data) {
-                        $scope.categorys = data;
-                    });
-                } else {
-                    $scope.tip = '该类别下有商品，无法删除！';
-                }
+            CategoryService.getAllCategoryInfo(function (data) {
+                $scope.categorys = data;
             });
-        };
+            $scope.tip = '';
 
-        $scope.updateClick = function (categoryInfo) {
-            $location.path('/category-update');
-            $location.search({'id': categoryInfo.id});
-        };
+            $scope.itemsPerPage = 5;
+            $scope.currentPage = 1;
 
-        $scope.addCategoryClick = function () {
-            $location.path('/category-add');
-        };
+            $scope.pageCount = function () {
+                var maxPage = Math.ceil($scope.categorys.length / $scope.itemsPerPage);
+                return maxPage;
+            };
+
+            $scope.range = function () {
+                var pages = [];
+                var maxPage = $scope.pageCount();
+                for (var i = 1; i <= maxPage; i++) {
+                    pages.push(i);
+                }
+                return pages;
+            };
+
+            $scope.prevPage = function () {
+                if ($scope.currentPage > 1) {
+                    $scope.currentPage--;
+                }
+            };
+
+            $scope.prevPageDisabled = function () {
+                return $scope.currentPage === 1 ? true : false;
+            };
+
+            $scope.nextPage = function () {
+                if ($scope.currentPage < $scope.pageCount()) {
+                    $scope.currentPage++;
+                }
+            };
+
+            $scope.nextPageDisabled = function () {
+                return $scope.currentPage === $scope.pageCount() ? true : false;
+            };
+
+            $scope.setPage = function (n) {
+                $scope.currentPage = n;
+            };
+
+
+            $scope.removeCategoryInfo = function (categoryInfo) {
+                CategoryService.removeCategoryInfo(categoryInfo, function (data) {
+                    if (data === 'success') {
+                        $scope.tip = '';
+                        CategoryService.getAllCategoryInfo(function (data) {
+                            $scope.categorys = data;
+                        });
+                    } else {
+                        $scope.tip = '该类别下有商品，无法删除！';
+                    }
+                });
+            };
+
+            $scope.updateClick = function (categoryInfo) {
+                $location.path('/category-update');
+                $location.search({'id': categoryInfo.id});
+            };
+
+            $scope.addCategoryClick = function () {
+                $location.path('/category-add');
+            };
+        }
     });
